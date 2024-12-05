0eae3d83373df3e45f7f4e809492a959e240ae31
AndreInda <andresinda@gmail.com>
2024-12-05 01:03:01 -0300

    modificado src/app/index.tsx-category/index.tsx-styes.ts

--- src/components/category/styles.ts
+++ src/components/category/styles.ts
@@ -1,15 +1,15 @@
-import { colors } from "@/src/styles/colors";
-import { StyleSheet } from "react-native";
+import { colors } from "@/src/styles/colors"
+import { StyleSheet } from "react-native"
 
 export const style = StyleSheet.create({
     container: {
         flexDirection: "row",
         alignItems: "center",
-        gap:5
+        gap:5,
     },
-    name:{
+
+    name: {
         fontSize: 16,
         fontWeight: "600",
-        color: colors.gray[400],
-    }
+    },
 })
